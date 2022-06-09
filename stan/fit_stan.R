9ae46b6f5540798c352e7e31371971ce5e0040ec
abriz97 <andreabrizzi@live.it>
2022-06-09 21:28:24 +0100

    Stan code

--- stan/fit_stan.R
+++ stan/fit_stan.R
@@ -1,13 +1,54 @@
 # AIMS:
-
+# prepare the datasets to run stan on
+# need to load data on:
+# mobility, UK government data, variant frequency
 
 ################
 # DEPENDENCIES #
 ################
 
-library(data.table)
 library(rstan)
+library(data.table)
 library(ggplot2)
+
+################
+#  ARGUMENTS   #
+################
+
+option_list <- list(
+  optparse::make_option(
+    c("c", "--country"),
+    type = "character",
+    default = NA_character_,
+    help = "Country for which to run analysis[must be one of England, Scotland, NothernIreland, Wales]",
+    dest = 'country'
+  ),
+  optparse::make_option(
+    c("--ratio_alpha_mortality"),
+    type = "numeric",
+    default = 1.6,
+    help = "Ratio between Fatality ratio of Alpha and Fatality Ratio of Wildtype[default=1.6]",
+    dest = "ratio_alpha_mortality"
+  ),
+  optparse::make_option(
+    "--max_date",
+    type = 'character',
+    default = '2021-04-01',
+    metavar = '"YYYY-MM-DD"',
+    help = 'Last date for analysis',
+    dest = 'max_date'
+  ),
+  optparse::make_option(
+    c("-f", "--figures"),
+    action = "store_true",
+    default = TRUE,
+    help = "Save Figures",
+    dest = "make.figures"
+  )
+)
+
+args <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
+args$max_date <- as.Date(args$max_date, format='%Y-%m-%d')
 
 ################
 #    PATHS     #
@@ -17,14 +58,17 @@ usr <- Sys.info()[['user']]
 if(usr == 'andrea')
 {
         indir <- '~/git/covid_mobility/'
-
 }else{
-
+        indir <- '~/git/covid_mobility/'
 }
 
 data.dir <- file.path(indir, 'data')
-path.who.deaths = file.path(data.dir, 'WHO_coviddeaths_bycountry_processed.csv')
-path.mobility.data = file.path(data.dir, 'apple_mobility_bycountry_processed.csv')
+results.dir <- file.path(indir, 'results')
+# path.who.deaths = file.path(data.dir, 'WHO_coviddeaths_bycountry_processed.csv')
+path.mobility.data.uk <- file.path(data.dir, 'apple_mobility_bycountry_processed_UK.csv')
+path.gov.data <- file.path(data.dir, 'UKgovernment_deaths_cases.csv')
+# make.figures <- 1
+
 
 ################
 #    HELPERS   #
@@ -45,58 +89,236 @@ path.mobility.data = file.path(data.dir, 'apple_mobility_bycountry_processed.csv
         DT
 }
 
-make_dataset <- function( country_code, T=100)
+make_dataset <- function(country=args$country, max_date=args$max_date, ratio_alpha_mortality = args$ratio_alpha_mortality)
 {
 
-        .ma <- function(x, n = 5) stats::filter(x, rep(1 / n, n), sides = 2)
+        # ratio_alpha_mortality = 1.6
+        # country= 'England'
+
+        .convolve.past <- function(vec1, vec2)
+        {
+                L1 <- length(vec1)
+                L2 <- length(vec2)
+                out <- rep(NA, length(vec1))
+
+                vec1_pad <- c(rep(0, L2 - 1 ), vec1)
+                vec2_rev <- rev(vec2)
+                
+                for(i in seq_along(vec1))
+                {
+                        vec2_pad <- c(rep(0, i-1), vec2_rev, rep(0, L1 - i ) )
+                        out[i] <- as.numeric(vec1_pad %*% vec2_pad)
+                }
+
+                return(c(NA, out[-length(out)]))
+        }
+
+        .make_discrete_gamma_pmf <- function(mean, std)
+        {
+                # r uses scale rate parametrisation, where
+                # mean = shape / rate
+                # std = sqrt(shape) / rate
+                s = mean^2/std^2
+                r = mean/std^2
+
+                if(0) # test
+                {
+                        tmp <- rgamma(1000, shape=s, rate=r)
+                        mean(tmp); sqrt(var(tmp))
+                }
+                probs <- sapply(0:100, pgamma, shape=s, rate=r)
+                probs <- c(probs[-1], NA) - probs
+                idx <- which(1 - cumsum(probs) <= 10e-4)[1]
+                probs <- probs[1:idx]
+                probs/sum(probs)
+        }
+
+        .ma <- function(x, n = 5) as.numeric(stats::filter(x, rep(1 / n, n), sides = 2))
 
         # 
-        cols <- c('Country_code', 'date', 'New_deaths')
-        dcov_tmp <- dcovid[ Country_code == country_code , ..cols ]
+        cols <- c('Country', 'date', 'deaths', 'cumdeaths')
+        dcov_tmp <- dcovid[ Country == country , ..cols ]
 
-        cols <- c('Country_code', 'date', 'mobility', 'transportation_type')
-        dmob_tmp <- dmobility[ Country_code == country_code , ..cols ]
+        cols <- c('Country', 'date', 'mobility', 'transportation_type')
+        dmob_tmp <- dmobility[ Country == country, ..cols ]
 
         cat('Interpolation to fix NAs in mobility... \n')
         dmob_tmp[, mobility := zoo::na.approx(mobility) , by='transportation_type']
 
         cat('Taking averages of the 3 transportation type...\n')
-        dmob_tmp[ , mobility := mean(mobility), by='date' ]
-        dmob_tmp[, mobility := as.numeric(.ma(mobility))]
+        dmob_tmp <- dmob_tmp[ , list(mobility = mean(mobility)), by=c('Country', 'date') ]
+        dmob_tmp[, mobility := .ma(mobility)]
 
-        ddata <- merge(dmob_tmp, dcov_tmp, by=c('Country_code', 'date'))
+        # merge
+        ddata <- merge(dmob_tmp, dcov_tmp, by=c('Country', 'date'))
         ddata <- ddata[!is.na(mobility)]
-        ddata <- .truncate_before_n_consecutive_reports(ddata, 7)
-        
+
+        cat('Selecting study period...\n')
+        cat('Start epidemic from day of 30th cum death as in Flaxman et al...: ')
+        ddata <- ddata[cumdeaths >= 30]
+        cat(ddata[, as.character(min(date))], '\n')
+        cat('Truncating epidemic on:',  as.character(max_date), '\n')
+        ddata <- ddata[date <= max_date]
+
+        if(0)
+        {
+                ddata <- .truncate_before_n_consecutive_reports(ddata, 7)
+        }
+
+        # Select serial interval for wildtype and alpha
+        serial_interval <- list(wildtype=list(),alpha=list())
+        serial_interval$wildtype <- list(mean = 6.48, std=3.83)
+        serial_interval$alpha <-  list(mean = 6.48, std=3.83)
+
+        case2death_interval <- list(wildtype=list(),alpha=list())
+        case2death_interval$wildtype <- list(mean= 18.8, std=8.46)
+        case2death_interval$alpha <- list(mean= 18.8, std=8.46)
+
         stan_data <- list(
                 N = ddata[, .N],
-                deaths = ddata[, New_deaths] ,
-                mob = ddata[, mobility],
-                T = T
+                deaths = ddata[, deaths] ,
+                mob = ddata[, mobility/100]
         )
-        
-        cnd <- lapply(stan_data, length)
-        stopifnot(cnd$deaths == cnd$mob)
 
-        stan_data
+        # add distribution of interval from case to death
+        stan_data$h_wildtype <- .make_discrete_gamma_pmf(mean=case2death_interval$wildtype$mean,
+                                               std=case2death_interval$wildtype$std)
+        stan_data$L_h_wildtype <- length(stan_data$h_wildtype)
+
+        stan_data$h_alpha <- .make_discrete_gamma_pmf(mean=case2death_interval$alpha$mean,
+                                               std=case2death_interval$alpha$std)
+        stan_data$L_h_alpha <- length(stan_data$h_alpha)
+
+        # add serial interval distribution
+        stan_data$w_wildtype <- .make_discrete_gamma_pmf(mean=serial_interval$wildtype$mean,
+                                               std=case2death_interval$wildtype$std)
+        stan_data$L_w_wildtype <- length(stan_data$w_wildtype)
+
+        stan_data$w_alpha <- .make_discrete_gamma_pmf(mean=case2death_interval$alpha$mean,
+                                               std=case2death_interval$alpha$std)
+        stan_data$L_w_alpha <- length(stan_data$w_alpha)
+
+        # find estimated proportion of deaths per variant 
+        dalpha_tmp <- dalpha[Country==country,,]
+        dalpha_tmp <- merge(dalpha_tmp, 
+                            dcovid[, .(Country, date,cases)],
+                            by=c('Country', 'date'))
+
+        dalpha_tmp[, cases := .ma(cases, n=7)]
+        dalpha_tmp[!is.na(cases)]
+        ggplot(dalpha_tmp, aes(x=date, y=cases)) + geom_point()
+
+        dalpha_tmp <- dalpha_tmp[!is.na(cases)]
+        dalpha_tmp[, alpha_conv := .convolve.past(proportion*cases, stan_data$h_alpha )]
+        dalpha_tmp[, wildtype_conv := .convolve.past((1-proportion)*cases, stan_data$h_wildtype )]
+
+        dalpha_tmp[, tmp := ratio_alpha_mortality * alpha_conv / wildtype_conv ]
+        dalpha_tmp[, phi := 1/(1+tmp^(-1))]
+        dalpha_tmp[, tmp := NULL]
+
+        ggplot(dalpha_tmp, aes(x=date, y=phi)) + geom_line()
+
+        if(args$make.figures) # Show deaths attribution by variant
+        {
+
+                tmp <- merge(ddata, dalpha_tmp, by=c('Country','date'))
+                tmp <- tmp[, list(
+                                  proportion=proportion,
+                                  deaths_alpha = deaths * phi,
+                                  deaths_wildtype = deaths * (1 - phi)
+                                  ), by=c('Country', 'date')]
+
+                tmp <- melt(tmp, id.vars=c('Country' , 'date', 'proportion'),
+                            value.name='deaths',
+                            variable.name='variant')
+                tmp[, variant:=gsub('deaths_', '', variant)]
+                setkey(tmp, date)
+                tmp
+
+                p <- ggplot(tmp, aes(x=date)) +
+                        geom_bar(position='stack', stat='identity', aes(fill=variant, y=deaths)) + 
+                        geom_line(aes(x=date, y=proportion*max(ddata$deaths))) +
+                        scale_x_date(breaks='2 weeks' , date_labels = "%d-%b-%y") +
+                        scale_y_continuous('COVID-19 deaths reported by UK government',
+                                           sec.axis = sec_axis(~. /max(ddata$deaths), 
+                                                               name='Variant frequencies among reported cases',
+                                                               labels=scales::percent) ) + 
+                        theme_bw() + 
+                        theme(legend.position='bottom',
+                              axis.text.x=element_text(angle=45, vjust=0.5, hjust=0.5)) + 
+                        labs(fill='attributed variant', x='',
+                             title=paste0('Estimated deaths by variant: ', country))
+                p
+
+                filename=file.path(results.dir, 'deaths_attributed_to_variant.png')
+                ggsave(filename, p, w=10, h=8)
+        }
+
+        # merge with ddata to subset to correct time window
+        dalpha_tmp <- merge(ddata[, .(date)], dalpha_tmp, all.x=TRUE)
+
+        stan_data$phi <- dalpha_tmp$phi
+
+        # Some checks
+        cnd <- lapply(stan_data, length)
+        stopifnot( uniqueN(c(cnd$deaths, cnd$mob, cnd$phi)) == 1)
+        stopifnot( cnd$w_wildtype == stan_data$L_w_wildtype)
+        stopifnot( cnd$w_alpha == stan_data$L_w_alpha)
+        stopifnot( cnd$h_wildtype == stan_data$L_h_wildtype)
+        stopifnot( cnd$h_alpha == stan_data$L_h_alpha)
+ 
+        return(stan_data)
 }
 
 ################
 #     MAIN     #
 ################
 
-dcovid <- fread(path.who.deaths)
-dmobility <- fread(path.mobility.data)
+## GET DATA
+#__________
 
+# dcovid <- fread(path.who.deaths)
+dcovid <- fread(path.gov.data)
+dmobility <- fread(path.mobility.data.uk)
+setnames(dcovid, 'country', 'Country')
+setnames(dmobility, 'country', 'Country')
+
+# while waiting for alpha prevalence, assume it increases linearly from 0 to 1 linearly.
+dalpha <- dcovid[, .(Country, date, proportion=0)]
+dalpha[ date >= '2020-10-12', proportion:=NA]
+dalpha[ date >= '2021-03-15', proportion:=1]
+dalpha[ , proportion := zoo::na.approx(proportion) ]
+if(0) plot( dalpha$date, dalpha$proportion)
+
+
+
+## FIT STAN
+#__________
+
+stan_data <- make_dataset(country='England')
 
 stan.model.path <- file.path(indir, 'stan', 'mobility.stan')
 model <- stan_model(stan.model.path)
+fit <- sampling(model,
+                data=stan_data, 
+                chains = 1,
+                iter=4000,
+                sample_file='tmp.csv'
+)
 
-stan_data <- make_dataset('IT')
+prm <- grep('^R0|^beta|delta', names(fit), value=TRUE)
+plot(fit, pars = prm)
+plot(fit, pars = prm,
+     show_density = TRUE, ci_level = 0.5, fill_color = "purple")
+plot(fit, pars = prm[1],
+     plotfun = "hist", include = FALSE)
 
-stan_data
+plot(fit, pars = grep('R0', prm, value=TRUE),  
+     plotfun = "trace" , inc_warmup = TRUE)
+plot(fit, pars = grep('beta', prm, value=TRUE),  
+     plotfun = "trace" , inc_warmup = TRUE)
 
-fit <- sampling(mod, data=stan_data)
+plot(fit, pars = prm,
+     plotfun = "rhat") + ggtitle("Example of adding title to plot")
 
-
-
+summary(fit)
--- stan/mobility.stan
+++ stan/mobility.stan
@@ -2,71 +2,165 @@ data{
   int<lower=0> N;
   int<lower=0> deaths[N];
   real mob[N];
-  int T;
+  
+  //variant specific attributes
+  // h: dbution for interval from case to death.
+  int<lower=0>  L_h_wildtype;
+  real<lower=0> h_wildtype[L_h_wildtype];
+  int<lower=0>  L_h_alpha;
+  real<lower=0> h_alpha[L_h_alpha];
+  // w: serial interval distribution
+  int<lower=0>  L_w_wildtype;
+  real<lower=0> w_wildtype[L_w_wildtype];
+  int<lower=0>  L_w_alpha;
+  real<lower=0> w_alpha[L_w_alpha];
+
+  // proportion phi of alpha-attributable deaths
+  real<lower=0, upper=1> phi[N];
 }
 
 transformed data{
-  //pre-calculate values of the gamma pdf
-  real omega[N];
-  real h[N];
-  real omega0; 
-  real h0;
+
+  // attribute deaths to variants
+  real D_wildtype[N];
+  real D_alpha[N];
+ 
+  // dot product function does not really exist for real * integer....
   for (n in 1:N){
-    // convert mean and sd to alpha and beta
-    h[n] = exp(gamma_lpdf(n | (18.8)^2 / (8.46)^2, 18.8 / (8.46)^2)); 
-    omega[n] = exp(gamma_lpdf(n | (6.48)^2 / (3.83)^2, 6.48 / (3.83)^2)); 
+    D_alpha[n] =  deaths[n] * phi[n];
+    D_alpha[n] =  deaths[n] * (1-phi[n]);
   }
-  h0 = exp(gamma_lpdf(0 | (18.8)^2 / (8.46)^2, 18.8 / (8.46)^2)); 
-  omega0 = exp(gamma_lpdf(0 | (6.48)^2 / (3.83)^2, 6.48 / (3.83)^2));
+
+  //pre-calculate values of the gamma pdf
+  // int deaths0 = 0; // assume 0 dvectoreaths on day 0
+  // real omega[N];
+  // real h[N];
+  // real omega0; 
+  // real h0;
+  // for (n in 1:N){
+    // convert mean and sd to alpha and beta
+    // h[n] = exp(gamma_lpdf(n | (18.8)^2 / (8.46)^2, 18.8 / (8.46)^2)); 
+    // omega[n] = exp(gamma_lpdf(n | (6.48)^2 / (3.83)^2, 6.48 / (3.83)^2)); 
+  // }
+  // h0 = exp(gamma_lpdf(0 | (18.8)^2 / (8.46)^2, 18.8 / (8.46)^2)); 
+  // omega0 = exp(gamma_lpdf(0 | (6.48)^2 / (3.83)^2, 6.48 / (3.83)^2));
 }
 
 parameters{
-  real R01;
-  real R02;
-  real beta1;
-  real beta2;
+  real R0_wildtype;
+  real R0_alpha;
+  real beta_wildtype;
+  real beta_alpha;
   real<lower=0> delta;
 }
 
 transformed parameters{
-  real R[N];
-  real RD[N];
+  real R_wildtype[N];
+  real R_alpha[N];
+  real RD_wildtype[N];
+  real RD_alpha[N];
 
   for (n in 1:N){
-    if (n <= T){
-      R[n] = exp(log(R01) - beta1 * (1-mob[n]));
-    }
-    else {
-      R[n] = exp(log(R02) - beta2 * (1-mob[n]));
-    }
+    R_wildtype[n] = exp(log(R0_wildtype) - beta_wildtype * (1-mob[n]));
+    R_alpha[n] = exp(log(R0_alpha) - beta_alpha * (1-mob[n]));
   }
-  for (n in 1:N){
-    RD[n] = R01 * h[n]; // s = 0
-    for (s in 1:(n-1)){ // s = 1, ..., n-1
-      RD[n] += R[s] * h[n-s];
+
+
+  // what happens when n=1 below...
+  for (n in 1:N)
+  {
+    if(n < L_h_wildtype)
+    {
+      RD_wildtype[n] = R0_wildtype * h_wildtype[n]; 
+    } else {
+      RD_wildtype[n] = 0;
     }
-    RD[n] += R[n] * h0; // s = n
+
+    if(n>1)
+    {
+      for (s in 1:min(L_h_wildtype, n-1)){ 
+
+        RD_wildtype[n] += R_wildtype[n-s] * h_wildtype[s];
+      }
+    }
+    RD_wildtype[n] += 0; //R_wildtype[n] * h0; 
   }
+
+  // what happens when n=1 below...
+  for (n in 1:N)
+  {
+    if(n < L_h_alpha)
+    {
+      RD_alpha[n] = R0_alpha * h_alpha[n]; 
+    } else {
+      RD_alpha[n] = 0;
+    }
+
+    if(n>1)
+    {
+      for (s in 1:min(L_h_alpha, n-1)){ 
+        RD_alpha[n] += R_alpha[n-s] * h_alpha[s];
+      }
+    }
+    RD_alpha[n] += 0; //R_alpha[n] * h0; 
+  }
+  // for (n in 1:N){
+  //   // s = 0
+  //   RD_wildtype[n] = R0_wildtype * h_wildtype[n]; 
+  //   RD_alpha[n] = R0_alpha * h_alpha[n]; 
+  //   // s = 1, ..., n-1
+  //     RD_alpha[n] += R_alpha[s] * h_alpha[n-s];
+  //   }
+  //   // I think I am assuming the below is 0...
+  //   // s = n
+  //   RD_wildtype[n] += 0; //R_wildtype[n] * h0; 
+  //   RD_alpha[n] += 0; // R_alpha[n] * h0; 
+  // }
 }
 
 model{
-  real sumD_omega;
+  // really don t like the name, instead conv_D_w.
+  real conv_phiDw_wildtype;
+  real conv_phiDw_alpha;
+  int idx_wildtype;
+  int idx_alpha;
   
-  //Priors: 
-  R01 ~ uniform(0,5);
-  R02 ~ uniform(0,5);
-  beta1 ~ uniform(-100,100);
-  beta2 ~ uniform(-100,100);
+  // Priors: 
+  R0_wildtype ~ uniform(0,5);
+  R0_alpha ~ uniform(0,5);
+  beta_wildtype ~ uniform(-100,100);
+  beta_alpha ~ uniform(-100,100);
   delta ~ exponential(1);
   
-  //Model:
-  //need to include deaths[1]!
+  // Model:
+  // Do not consider the first deaths, there are no past deaths which 
+  //   the model can use to justify their existence.
+  // deaths[1] ~ neg_binomial_2(RD[1] * (deaths0 * omega[1] + deaths[1] * omega0), delta);
+  //
   for (n in 2:N){
-    sumD_omega = deaths[n] * omega0;
-    for (s in 1:(n-1)){
-      sumD_omega += deaths[s] * omega[n-s];
+    // sumD_omega = deaths0 * omega[n]; // s = 0
+    // initialise convolutions values
+    conv_phiDw_wildtype = 0;
+    conv_phiDw_alpha = 0;
+    idx_wildtype = min(L_w_wildtype,  n-1 );
+    idx_alpha = min(L_w_alpha,  n-1 );
+    
+    for (s in 1:idx_wildtype){ // s = 1, ..., n-1
+      conv_phiDw_wildtype += D_wildtype[n-s] * w_wildtype[s];
     }
-    deaths[n] ~ neg_binomial_2(RD[n] * sumD_omega, delta);
+
+    for (s in 1:idx_alpha){ // s = 1, ..., n-1
+      conv_phiDw_alpha += D_alpha[n-s] * w_alpha[s];
+    }
+
+    // no no no: w[0] should be 0
+    // sumD_omega += deaths[n] * omega0; // s = n
+    // deaths[n] ~ neg_binomial_2(RD_wildtype[n] * conv_phiDw_wildtype + RD_alpha[n] * conv_phiDw_alpha , delta);
+
   }
 }
 
+generated quantities {
+
+
+}
